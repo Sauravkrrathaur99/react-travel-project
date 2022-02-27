c774e85a7ce2fb4b2b3eba35b7631b1852abd8c6
Saurav Kumar Rathaur <71010336+Sauravkrrathaur99@users.noreply.github.com>
2022-02-28 01:20:24 +0530

    Updates

---
+++ precache-manifest.ca0a82823f3d55979bbe7146ac376d2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbf04afa3509242b771c",
+    "url": "./static/css/main.51ebeea5.chunk.css"
+  },
+  {
+    "revision": "cbf04afa3509242b771c",
+    "url": "./static/js/main.7e83a245.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ca8d26db636344a9607d",
+    "url": "./static/js/2.1675f732.chunk.js"
+  },
+  {
+    "revision": "e1f4326f7e6a9f9134ba747a2b526b65",
+    "url": "./static/media/img-8.e1f4326f.jpg"
+  },
+  {
+    "revision": "36d5a87c511bab9639ad7750ffd9052c",
+    "url": "./static/media/img-1.36d5a87c.jpg"
+  },
+  {
+    "revision": "3df2bb986be6e43d8efda327b7e5c524",
+    "url": "./static/media/img-2.3df2bb98.jpg"
+  },
+  {
+    "revision": "314da2d362bd18f6d13101c5732d8a0d",
+    "url": "./static/media/img-9.314da2d3.jpg"
+  },
+  {
+    "revision": "64272f77efb5cee2cbb55e9314f04904",
+    "url": "./static/media/img-4.64272f77.jpg"
+  },
+  {
+    "revision": "674ce46e7138eab0f7c7e2dbe9792d70",
+    "url": "./static/media/img-3.674ce46e.jpg"
+  },
+  {
+    "revision": "faad3897e430f8cc6cad540fe6b3674a",
+    "url": "./static/media/img-home.faad3897.jpg"
+  },
+  {
+    "revision": "2485e8995ed6b70b9bef28d6dd56a2ff",
+    "url": "./index.html"
+  }
+];
